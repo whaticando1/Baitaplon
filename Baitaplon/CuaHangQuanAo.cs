092bd6ce80f63b883e3b7e0b8056900766d689c1
verysadboia-cmd <verysadboia@gmail.com>
2025-11-11 21:18:02 +0700

    q

--- Baitaplon/CuaHangQuanAo.cs
+++ Baitaplon/CuaHangQuanAo.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Baitaplon
 {
-    public partial class Form1 : Form
+    public partial class CuaHangQuanAo : Form
     {
-        public Form1()
+        public CuaHangQuanAo()
         {
             InitializeComponent();
         }
--- Baitaplon/Program.cs
+++ Baitaplon/Program.cs
@@ -16,7 +16,7 @@ namespace Baitaplon
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new CuaHangQuanAo());
         }
     }
 }
