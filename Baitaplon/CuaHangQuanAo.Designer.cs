96b2906c58870b02873dcb9a0ba3eadfd4a95701
verysadboia-cmd <verysadboia@gmail.com>
2025-11-11 21:35:33 +0700

    w

--- Baitaplon/CuaHangQuanAo.Designer.cs
+++ Baitaplon/CuaHangQuanAo.Designer.cs
@@ -33,20 +33,20 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(282, 164);
+            this.button1.Location = new System.Drawing.Point(1057, 40);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(112, 36);
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // CuaHangQuanAo
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(823, 471);
+            this.ClientSize = new System.Drawing.Size(1234, 736);
             this.Controls.Add(this.button1);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "CuaHangQuanAo";
             this.Text = "Form1";
             this.ResumeLayout(false);
